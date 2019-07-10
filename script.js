7f22ffcd7debfe156c060bcbf89194fdb08548b6
Adriano-Defilippis <ciprianoadrianodefilippis@gmail.com>
2019-07-10 09:31:26 +0200

    Lo script finisce nel momento in cui l'utente digita un numero presente nell'array generato dal pc

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 // Con difficoltà 0=> da 1 a 100, con difficoltà 1 => da 1 a 80 con difficoltà 2=> da 1 a 50
 
 
+
 // Creo la Funzione per Generare numeri random, e li pusha in un Array, il range e la quantità da generare le imposto con variabili locali
 function generatoreRandom(minOutput, maxOutput, min, max, array){
 
@@ -30,59 +31,67 @@ array = [];
 }
 
 
-// Questa funzione deve chiedere un numero da 1 a 100 fino a quando l'utente non avrà digitato un numero presente nell'array generato in precedenza
-function inputUtente(array, input){
 
-  var i = 0;
+// Funzione per chiedere massimo 20 input all'numUtente
+function tentativiMax(max, array){
 
-  while(i<array.length){
+  var max;
 
-    var penetrazion = true;
+  console.log(array.join(","));
 
-    while(penetrazion == true){
-      numUtente;
-      if (input == array[i]) {
-        penetrazion = false;
-        console.log("mina  ", array[i]);
-      }
 
-      console.log("non mina");
+  for (i = 0; i <= max; i++){
 
-      return true;
-    }
+    input = parseInt(prompt('Inserisci un numero da 1 a 100. Attento alle mine!'));
 
-    console.log("Stampo elementi Array ", array[i]);
 
-    i++
   }
 
 }
 
+function in_array(input, a){
+    //input => VALORE CERCATO
+    //a => ARRAY
+
+    for(var i = 0; i < a.length; i++) {
+      if(a[i] == input) return true;
+    }
+    return false;
+  }
 
 
 
+
+
+
+// Esecuzione del codice
 var sequenzaRandom = generatoreRandom(1, 16, 1, 100);
 
+var input = parseInt(prompt('Inserisci un numero da 1 a 100. Attento alle mine!'));
 
-
-var numUtente = parseInt(prompt('Inserisci un numero da 1 a 100. Attento alle mine!'));
-
-// inputUtente(numUtente, sequenzaRandom);
-
-console.log("Numero inserito dall'utente ",numUtente);
 
 // Log dei numeri generati in base alla funzione generatoreRandom;
 console.log("Array generato dal pc ", sequenzaRandom);
 
 console.log("Lunghezza array generato dal pc ", sequenzaRandom.length);
 
-inputUtente(sequenzaRandom, numUtente);
 
+var penetrazion = true;
 
+// Ciclo che mi permette di uscire dalla richiesta input se il numero inserito è presente nell'array generato dal pc
+while (penetrazion == true){
 
+  input = parseInt(prompt('Inserisci un numero da 1 a 100. Attento alle mine!'));
 
+  if(in_array(input, sequenzaRandom)) {
+    alert('presente');
 
+    console.log(input, ' presente');
 
-
-
-// inputUtente(numUtente, sequenzaRandom);
+    penetrazion = false;
+  }else{
+    alert('elemento non trovato');
+    console.log(input, ' elemento non trovato');
+    penetrazion = true;
+  }
+}
